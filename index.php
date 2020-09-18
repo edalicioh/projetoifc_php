332cf7fe569b5bd7344c50bde70c173b81c549c2
microtarget <edalicio@outlook.com>
2020-09-18 13:37:30 -0300

    master de heroku

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Headers: X-Requested-With, Content-Type, Origin, Cache-Control, Pragma, Authorization, Accept, Accept-Encoding');
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: X-Requested-With, Content-Type, Origin, Cache-Control, Pragma, Authorization, Accept, Accept-Encoding");
 
-require_once __DIR__ . '/vendor/autoload.php';
-require_once __DIR__ . '/core/bootstrap.php';
+require_once __DIR__ ."/config.php";
+require_once __DIR__."/vendor/autoload.php";
+require_once __DIR__."/core/bootstrap.php";
