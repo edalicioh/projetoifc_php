ae4af9366564fe1e422bcdedfd495b1a16dfc1a3
microtarget <edalicio@outlook.com>
2020-09-18 14:31:25 -0300

    master de heroku

--- index.php
+++ index.php
@@ -2,6 +2,9 @@
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Headers: X-Requested-With, Content-Type, Origin, Cache-Control, Pragma, Authorization, Accept, Accept-Encoding');
 
+var_dump(getenv('JAWSDB_URL'));
+die;
+
 require_once __DIR__ . '/config.php';
 require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/core/bootstrap.php';
